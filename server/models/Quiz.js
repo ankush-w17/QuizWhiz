83c07541258e004ea28aa7fa126a781ad2ce9690
Ankush <ankushwadhwani222@gmail.com>
2025-10-02 22:42:38 +0530

    initialized db

---
+++ server/models/Quiz.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+const quizSchema = new mongoose.Schema({
+  topic: {
+    type: String,
+    required: true,
+    trim: true  
+  },
+  questions: [{
+    question: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: function(arr) {
+          return arr.length === 4;  
+        },
+        message: 'Each question must have exactly 4 options'
+      }
+    },
+    correctAnswer: {
+      type: String,
+      required: true
+    }
+  }],
+  shareableCode: {
+    type: String,
+    required: true,
+    unique: true  
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now  
+  }
+});
+
+
+module.exports = mongoose.model('Quiz', quizSchema);
