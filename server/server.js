dff956342352e5a37e9fe230abee8190d9c2b17c
Ankush <ankushwadhwani222@gmail.com>
2025-10-06 22:19:54 +0530

    edited cookie setting again

--- server/server.js
+++ server/server.js
@@ -322,6 +322,8 @@ app.post("/api/auth/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
